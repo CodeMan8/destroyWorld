501eceb0f7a97edcb0ecfb8e2f3956b253b8a564
Bartu akman <bartuakman@Bartu-MacBook-Air.local>
2018-09-06 09:33:21 +0300

    hello wo

--- DestroyWorld/ViewController.swift
+++ DestroyWorld/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Yes finally")
+        print("Oh okey")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
